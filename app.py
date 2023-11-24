3213cc335e6b35526fb013bdd00145d738b20df7
20127045- Võ Đình Khôi <96615959+vdkhoi20@users.noreply.github.com>
2023-11-24 18:24:01 +0700

    change to dark mode

--- app.py
+++ app.py
@@ -1,16 +1,17 @@
 import gradio as gr
 from gradio_app.Chatbot_tab import create_Chatbot_tab
-from gradio_app.Generate_Image_Disease_tab import create_Generate_Image_Disease_tab
+# from gradio_app.Generate_Image_Disease_tab import create_Generate_Image_Disease_tab
 from gradio_app.X_Quang_tab import create_X_Quang_tab
 from gradio_app.Recognition_Skin_Disease_tab import create_Recognition_Skin_Disease_tab
 
 
 
 demo=gr.TabbedInterface([create_Chatbot_tab(),
-                            create_Generate_Image_Disease_tab(),
+                            create_X_Quang_tab(),#create_Generate_Image_Disease_tab(), tắt cái của Thành để chạy mấy cái khác khỏi load model
                             create_X_Quang_tab(),
                             create_Recognition_Skin_Disease_tab()],
-                        ["Health consulting assistant","Generate images of the disease","X-ray diagnosis","skin diseases recognition"])
-demo.launch()
+                        ["Health consulting assistant","Generate images of the disease","X-ray diagnosis","skin diseases recognition"],
+                        title="Healthcare AI Application",
+                        theme="abidlabs/dracula_revamped")
 if __name__ == "__main__":
     demo.queue().launch(share=True)
