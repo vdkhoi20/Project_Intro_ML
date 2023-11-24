988a90f696e2660be94127453b6edd0a86f5779b
Đỗ Đạt Thành <79926672+datthanh1942@users.noreply.github.com>
2023-11-24 10:22:38 +0700

    Update Generate_Image_Disease_tab.py
    Call model generate image from text + API translate to English + theme for gradio app

--- gradio_app/Generate_Image_Disease_tab.py
+++ gradio_app/Generate_Image_Disease_tab.py
@@ -1,13 +1,28 @@
 import gradio as gr
+from diffusers import DiffusionPipeline
+import torch
+from torchvision.transforms import ToPILImage
+from deep_translator import GoogleTranslator
+
+# Load the pretrained model
+pipe = DiffusionPipeline.from_pretrained("stabilityai/stable-diffusion-xl-base-1.0", torch_dtype=torch.float16, use_safetensors=True, variant="fp16")
+pipe.to("cuda") # if using torch < 2.0
+
 def create_Generate_Image_Disease_tab():
-    with gr.Blocks() as Generate_Image_Disease_Tab:
-        def update(name):
-            return f"Welcome to Gradio, {name}!"
+    with gr.Blocks(theme='abidlabs/dracula_revamped') as Generate_Image_Disease_Tab:
+        def generate(prompt):
+            # Translate the prompt from Vietnamese to English
+            prompt_en = GoogleTranslator(source='vietnamese', target='english').translate(prompt)
+
+            # Generate images from the model
+            images = pipe(prompt=prompt_en).images[0]
+
+            return images
 
         gr.Markdown("Start typing below and then click **Run** to see the output.")
         with gr.Row():
-            inp = gr.Textbox(placeholder="What is your name?")
-            out = gr.Textbox()
+            inp = gr.Textbox(placeholder="Description")
+            out = gr.Image()
         btn = gr.Button("Run")
-        btn.click(fn=update, inputs=inp, outputs=out)
+        btn.click(fn=generate, inputs=inp, outputs=out)
     return Generate_Image_Disease_Tab
