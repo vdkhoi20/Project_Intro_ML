cbbe5ff3f0e17ed0c5f10bfd05f4cbe9f585faf7
20127045- Võ Đình Khôi <96615959+vdkhoi20@users.noreply.github.com>
2023-11-25 13:36:22 +0700

    VietNamese

--- app.py
+++ app.py
@@ -10,8 +10,11 @@ demo=gr.TabbedInterface([create_Chatbot_tab(),
                             create_X_Quang_tab(),#create_Generate_Image_Disease_tab(), tắt cái của Thành để chạy mấy cái khác khỏi load model
                             create_X_Quang_tab(),
                             create_Recognition_Skin_Disease_tab()],
-                        ["Health consulting assistant","Generate images of the disease","X-ray diagnosis","skin diseases recognition"],
-                        title="Healthcare AI Application",
+                        ["Trợ Lí AI Tư Vấn Sức Khỏe",
+                         "Sinh Hình Ảnh Về Bệnh Ngoài Da",
+                         "Chẩn Đoán X-Quang Viêm Phổi",
+                         "Chẩn Đoán Bệnh Ngoài Da"],
+                        title="Ứng Dụng AI Cho Vấn Đề Y Tế Gia Đình",
                         theme="HaleyCH/HaleyCH_Theme")#abidlabs/dracula_revamped")
 if __name__ == "__main__":
     demo.queue().launch(share=True)
