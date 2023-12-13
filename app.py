ae456064ec42577199453a88d7034d4012a71e99
20127045- Võ Đình Khôi <96615959+vdkhoi20@users.noreply.github.com>
2023-12-13 23:47:43 +0700

    Update app.py
    complete code

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
 import gradio as gr
 from gradio_app.Chatbot_tab import create_Chatbot_tab
-# from gradio_app.Generate_Image_Disease_tab import create_Generate_Image_Disease_tab
+from gradio_app.Generate_Image_Disease_tab import create_Generate_Image_Disease_tab
 from gradio_app.X_Quang_tab import create_X_Quang_tab
 from gradio_app.Recognition_Skin_Disease_tab import create_Recognition_Skin_Disease_tab
 
 
 
 demo=gr.TabbedInterface([create_Chatbot_tab(),
-                            create_X_Quang_tab(),#create_Generate_Image_Disease_tab(), tắt cái của Thành để chạy mấy cái khác khỏi load model
+                            create_Generate_Image_Disease_tab(), #tắt cái của Thành để chạy mấy cái khác khỏi load model
                             create_X_Quang_tab(),
                             create_Recognition_Skin_Disease_tab()],
                         ["Trợ Lí AI Tư Vấn Sức Khỏe",
@@ -17,4 +17,4 @@ demo=gr.TabbedInterface([create_Chatbot_tab(),
                         title="Ứng Dụng AI Cho Vấn Đề Y Tế Gia Đình",
                         theme="HaleyCH/HaleyCH_Theme")#abidlabs/dracula_revamped")
 if __name__ == "__main__":
-    demo.queue().launch(share=True)
+    demo.launch(share=True)
