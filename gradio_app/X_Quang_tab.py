b7477a35dbb72e1a56fcf8e3287ae21b8db1ad12
20127045- Võ Đình Khôi <96615959+vdkhoi20@users.noreply.github.com>
2023-12-14 11:01:12 +0700

    Update X_Quang_tab.py
    fix bug x quang

--- gradio_app/X_Quang_tab.py
+++ gradio_app/X_Quang_tab.py
@@ -1,10 +1,11 @@
 import gradio as gr
 from PIL import Image
 from ultralyticsplus import YOLO, postprocess_classify_output
+import os
+model = YOLO('yolov8n.pt')
+model.overrides['conf'] = 0.25
 
 def update_X_Quang(image):
-    model = YOLO('keremberke/yolov8m-chest-xray-classification')
-    model.overrides['conf'] = 0.25
     results = model.predict(image)
     processed_result = postprocess_classify_output(model, result=results[0])
     normal = processed_result['NORMAL']
@@ -16,14 +17,11 @@ def update_X_Quang(image):
         result = "Có bệnh viêm phổi"
         score = round(pneumonia*100)
     return result + " với độ chính xác là: " + str(score) + "%"
-    return processed_result
 
 def create_X_Quang_tab():
     with gr.Blocks() as X_Quang_tab:
-        def update(name):
-            return f"Welcome to Gradio, {name}!"
         with gr.Row():
-            inp = gr.Image(label="Ảnh X-Quang chuẩn đoán viêm phổi")
+            inp = gr.Image(label="Ảnh X-Quang chuẩn đoán viêm phổi",value=os.path.join(os.path.dirname(__file__), "image_X_Quang.jpg"), interactive=True, height=512, width=512)
         with gr.Row():
             out = gr.Text(label="Kết quả X-Quang viêm phổi")
         btn = gr.Button("Chạy")
